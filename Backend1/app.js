4d5c67abc4ac9eca5b529cbdb413809b66bfebb1
dev-gurjar <dev.viga@gmail.com>
2024-02-03 04:59:07 +0530

    asdfs

--- Backend1/app.js
+++ Backend1/app.js
@@ -1,16 +1,15 @@
 const express = require('express');
-
-const app = express()
-const PORT = 8000
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 app.get('/about', (req, res) => {
-  res.send('About route 🎉 ')
-})
+  res.send('About route 🎉 ');
+});
 
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
-})
+});
